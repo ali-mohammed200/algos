b0eb0b1a18fbb75f84ce8b1edeff1471a72418ad
Mohammad Ali <35857177+ali-mohammed200@users.noreply.github.com>
2020-06-12 01:37:20 -0500

    update

--- codewars/domain.js
+++ codewars/domain.js
@@ -33,3 +33,44 @@ function domainName(url){
     return domain = base(url, start);
   }
 }
+
+// function domainName(url){
+//   let domain = "";
+//   let start = 0;
+  
+//   function base(url, start){
+//      let domain = '';
+//      while(start < url.length){
+//       if(url[start] === "."){
+//         break;
+//        }
+//       domain += url[start]
+//       start++;
+//     }
+//     return domain;
+//   }
+  
+//   if(url[0] === "h"){
+//     if(url[11] === "." && (11+4) === url.length){
+//       domain = domain
+//     }else if(url[10] === "."){
+//       return domain = base(url, 11);
+//     }else if(url[11] === "."){
+//       return domain = base(url, 12);
+//     }
+//     if(url[7] === "/" && url[4] === "s"){
+//       return domain = base(url, 8);
+//     }else if(url[6] === "/"){
+//       return domain = base(url, 7);
+//     }
+//   }else if(url[0] === "w"){
+//     return domain = base(url, 4);
+//   }else if(url[0] === "/"){
+//     return domain = base(url, 2);
+//   }else{
+//     return domain = base(url, start);
+//   }
+// }
+
+
+// // Test.assertEquals(domainName("https://123.net"), "123");
